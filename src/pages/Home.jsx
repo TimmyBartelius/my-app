535152c7afb0a87042b9db9a2140a7df56ee1b4b
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-05-19 13:16:20 +0200

    Sökväg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "./components/firebase";
 import "./components/Home.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";

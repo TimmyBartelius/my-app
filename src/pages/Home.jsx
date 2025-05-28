8a0a18fde9bdb1b6639a68940b23b5a3986649d4
Timmy Bartelius <timmy.bartelius1@gmail.com>
2025-05-28 11:53:27 +0200

    Varför?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../components/firebase";
 import "../components/Home.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 import AllToys from "../components/Firebase/AllToys";
 
